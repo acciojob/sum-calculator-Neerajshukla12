7a4e31dae7eb7a28b5e0af7e34533681741a996a
Neeraj <shukla7071@gmail.com>
2025-12-26 13:56:54 +0530

    try2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./../styles/App.css";
 
 const App = () => {
+  const [numbers, setNumbers] = useState(0);
   const [sum, setSum] = useState(0);
 
   function handleInput(e) {
     const digit = Number(e.target.value);
-    setSum(prev => prev + digit);
+    if (!isNaN(digit)) {
+      setNumbers(prev => [...prev, digit]);
+    }
   }
+
+  useEffect(() => {
+    const calculateSum=() => {
+      const total = numbers.reduce((acc, val) => acc + val, 0);
+      setSum(total);
+    };
+
+    calculateSum();
+  }, [numbers]);
 
   return (
     <div>
